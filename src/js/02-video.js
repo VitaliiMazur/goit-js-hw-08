0f8eb2225ace08280f2393b22ca2fb13bd83470f
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-24 18:15:31 +0300

    завдання 2 зроблене

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+iframePlayer.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(params) {
+  localStorage.setItem('videoplayer-current-time', params.seconds);
+}
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+console.log(saveTime);
+
+iframePlayer
+  .setCurrentTime(saveTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
