8d66b547df0eb91ea4a7ba8cd255555c1dce6e08
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-24 10:21:12 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+const creatImgGallery = createGalleryBox(galleryItems);
+
+galleryBox.insertAdjacentHTML('beforeend', creatImgGallery);
+
+function createGalleryBox(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
