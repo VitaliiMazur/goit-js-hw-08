3e617690fe44e7481db76387701c0ce4f3db4bc6
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-08-26 20:35:50 +0300

    зробив 3тє завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 import throttle from 'lodash.throttle';
+
+import { localValue } from './localStorageValue';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackformData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+  input: document.querySelector('input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormInput(evt) {
+  feedbackformData[evt.target.name] = evt.target.value;
+  const storageData = JSON.stringify(feedbackformData);
+  localStorage.setItem(STORAGE_KEY, storageData);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  const savedStoragedata = localValue(STORAGE_KEY, '');
+  console.log(savedStoragedata);
+  if (savedStoragedata) {
+    refs.input.value = savedStoragedata.email;
+    refs.textarea.value = savedStoragedata.message;
+  }
+}
---
+++ src/js/localStorageValue.js
@@ -0,0 +1,12 @@
+export function localValue(key, initValue) {
+  const value = localStorage.getItem(key);
+  if (value) {
+    try {
+      return JSON.parse(value) || '';
+    } catch (err) {
+      console.log('invalid value', err);
+      localStorage.removeItem(key);
+    }
+  }
+  return initValue;
+}
